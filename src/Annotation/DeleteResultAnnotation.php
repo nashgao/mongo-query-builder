158f24730172750d738648fa3af340a556edac1f
nashgao <nashgao1992@gmail.com>
2021-02-05 15:49:59 +1000

    first commit

---
+++ src/Annotation/DeleteResultAnnotation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (C) SPACE Platform PTY LTD - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nash Gao <nash@spaceplaform.co>
+ * @organization Space Platform
+ * @project space-webapp-server-hyperf
+ * @create Created on 2020/9/11 上午2:05
+ * @author Nash Gao
+ */
+
+declare(strict_types=1);
+
+
+namespace App\Annotation;
+
+use Hyperf\Di\Annotation\AbstractAnnotation;
+
+
+/**
+ * @Annotation
+ * @Target("METHOD")
+ */
+class DeleteResultAnnotation extends AbstractAnnotation
+{
+
+}
---
+++ src/Annotation/FindOneResultAnnotation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright (C) SPACE Platform PTY LTD - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nash Gao <nash@spaceplaform.co>
+ * @organization Space Platform
+ * @project space-webapp-server-hyperf
+ * @create Created on 2020/9/11 上午11:57
+ * @author Nash Gao
+ */
+
+declare(strict_types=1);
+
+
+namespace App\Annotation;
+
+use Hyperf\Di\Annotation\AbstractAnnotation;
+
+/**
+ * @Annotation
+ * @Target("METHOD")
+ */
+class FindOneResultAnnotation extends AbstractAnnotation
+{
+
+}
